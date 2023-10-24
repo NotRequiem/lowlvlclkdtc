0ac226bb6b5711d190a0db19e1c8adfb267fee0b
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-10-24 18:55:55 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+HHOOK hHook;
+DWORD StartTime;
+int wParamCounts[8] = {0};
+int maxWParam = 0;
+DWORD lastClickTime = 0;
+
+LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode >= 0) {
+        if (wParam != 512 && wParam != 514 && wParam != 517 && wParam != 520 && wParam != 522 && wParam != 524 && wParam != 526 && wParam != 675) {
+            wParamCounts[wParam]++;
+        }
+    }
+
+    DWORD CurrentTime = GetTickCount64();
+    DWORD ElapsedTime = CurrentTime - StartTime;
+
+    if (ElapsedTime <= 5000) {
+        if (wParamCounts[wParam] > wParamCounts[maxWParam]) {
+            maxWParam = wParam;
+        }
+    } else {
+        if (nCode >= 0 && wParam == maxWParam) {
+            if (lastClickTime != 0) {
+                DWORD currentClickTime = GetTickCount64();
+                printf("Detected attack control: (%d). Delay between clicks: %d ms\n", maxWParam, currentClickTime - lastClickTime);
+            }
+            lastClickTime = GetTickCount64();
+        }
+    }
+
+    return CallNextHookEx(hHook, nCode, wParam, lParam);
+}
+
+void UninstallHook() {
+    if (hHook != NULL) {
+        if (!UnhookWindowsHookEx(hHook)) {
+            printf("Error uninstalling mouse hook. Error code: %d\n", GetLastError());
+        }
+        hHook = NULL;
+    }
+}
+
+bool InstallHook() {
+    hHook = SetWindowsHookEx(WH_MOUSE_LL, (HOOKPROC)MouseProc, GetModuleHandle(NULL), 0);
+    if (!hHook) {
+        printf("Error installing mouse hook. Error code: %d\n", GetLastError());
+        UninstallHook();
+        return false;
+    }
+
+    printf("Hook installed successfully.\n");
+    return true;
+}
+
+int main() {
+    StartTime = GetTickCount64();
+    InstallHook();
+
+    MSG msg;
+
+    while (1) {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        DWORD CurrentTime = GetTickCount64();
+        DWORD ElapsedTime = CurrentTime - StartTime;
+        if (ElapsedTime >= 10000) {
+            break;
+        }
+    }
+
+    UninstallHook();
+
+    return 0;
+}
